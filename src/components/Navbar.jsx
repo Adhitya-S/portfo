49a5b3862d2ec02a61e8208fea427928630b667b
Adhitya S <adhityavalli@gmail.com>
2025-06-12 10:19:14 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Adhitya S &nbsp;
-            <span className='sm:block hidden'> | Software Developer With Full Stack</span>
+            <span className='sm:block hidden'> | AI & Data Science Engineer</span>
           </p>
         </Link>
 
